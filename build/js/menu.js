0a23b4396eb86f0773ffe3db9a8c50441c20d31d
Chris Humboldt <chrismodem@gmail.com>
2017-03-08 16:49:39 +0200

    Slide menu done.

---
+++ build/js/menu.js
@@ -0,0 +1,100 @@
+Rocket.defaults.menu = {
+    closeText: 'close',
+    reveal: 'left',
+    type: 'slide'
+};
+var RockMod_Menu;
+(function (RockMod_Menu) {
+    var _RD = Rocket.defaults.menu;
+    var reveals = ['_r-left', '_r-right'];
+    var types = ['_t-contextual', '_t-slide'];
+    var menu = {
+        close: function (callback) {
+            if (callback === void 0) { callback = null; }
+            var openMenu = Rocket.dom.element('.rocket-menu._reveal');
+            Rocket.classes.remove(Rocket.dom.html, 'rme-reveal');
+            if (Rocket.exists(openMenu) && Rocket.is.element(openMenu)) {
+                if (Rocket.has.class(openMenu, '_t-slide')) {
+                    Rocket.overlay.hide();
+                }
+                Rocket.classes.remove(openMenu, '_reveal');
+            }
+            if (Rocket.is.function(callback)) {
+                return callback();
+            }
+        },
+        globalSetup: function () {
+            Rocket.event.add(Rocket.dom.element('#rocket-overlay'), 'click', menu.close);
+        },
+        setup: function (options) {
+            var thisMenu = Rocket.dom.element(options.target);
+            var triggers = Rocket.dom.select(options.triggers);
+            if (!Rocket.is.element(thisMenu) || triggers.length < 1 || types.indexOf("_t-" + options.type) < 0) {
+                return false;
+            }
+            function menuShow(event) {
+                if (event === void 0) { event = null; }
+                if (event) {
+                    event.preventDefault();
+                }
+                menu.close(function () {
+                    if (options.type === 'slide') {
+                        Rocket.overlay.show();
+                    }
+                    Rocket.classes.add(Rocket.dom.html, 'rme-reveal');
+                    Rocket.classes.add(thisMenu, '_reveal');
+                });
+            }
+            Rocket.classes.remove(thisMenu, reveals.concat(types));
+            switch (options.type) {
+                case 'contextual':
+                    Rocket.classes.add(thisMenu, "_t-" + options.type);
+                    break;
+                case 'slide':
+                    Rocket.classes.add(thisMenu, "_t-" + options.type + " _r-" + options.reveal);
+                    if (!Rocket.exists(thisMenu.querySelector('a.rme-close-link'))) {
+                        var closeLink = document.createElement('a');
+                        Rocket.classes.add(closeLink, 'rme-close-link');
+                        closeLink.href = '';
+                        closeLink.innerHTML = options.closeText;
+                        thisMenu.appendChild(closeLink);
+                    }
+                    break;
+            }
+            var menuLinks = thisMenu.querySelectorAll('a');
+            for (var _i = 0, menuLinks_1 = menuLinks; _i < menuLinks_1.length; _i++) {
+                var menuLink = menuLinks_1[_i];
+                Rocket.event.add(menuLink, 'click', function (event) {
+                    if (Rocket.has.class(event.currentTarget, 'rme-close-link')) {
+                        event.preventDefault();
+                    }
+                    menu.close();
+                });
+            }
+            for (var _a = 0, triggers_1 = triggers; _a < triggers_1.length; _a++) {
+                var trigger = triggers_1[_a];
+                Rocket.event.add(trigger, 'click', menuShow);
+            }
+            return {
+                menu: thisMenu,
+                show: menuShow
+            };
+        }
+    };
+    function init(uOptions) {
+        if (!Rocket.is.object(uOptions)) {
+            uOptions = {};
+        }
+        var options = {
+            closeText: Rocket.helper.setDefault(uOptions.closeText, _RD.closeText),
+            reveal: Rocket.helper.setDefault(uOptions.reveal, _RD.reveal),
+            triggers: Rocket.helper.setDefault(uOptions.triggers, ''),
+            target: Rocket.helper.setDefault(uOptions.target, ''),
+            type: Rocket.helper.setDefault(uOptions.type, _RD.type)
+        };
+        return menu.setup(options);
+    }
+    RockMod_Menu.init = init;
+    menu.globalSetup();
+})(RockMod_Menu || (RockMod_Menu = {}));
+Rocket.menu = RockMod_Menu.init;
---
+++ build/ts/menu.ts
@@ -0,0 +1,111 @@
+/*
+Author: Chris Humboldt
+*/
+
+// Extend Rocket
+Rocket.defaults.menu = {
+   closeText: 'close',
+   reveal: 'left',
+   type: 'slide'
+};
+
+// Module
+module RockMod_Menu {
+   // Variables
+   const _RD = Rocket.defaults.menu;
+   const reveals = ['_r-left', '_r-right'];
+   const types = ['_t-contextual', '_t-slide'];
+
+   // Functions
+   const menu = {
+      close: (callback = null) => {
+         let openMenu = Rocket.dom.element('.rocket-menu._reveal');
+
+         Rocket.classes.remove(Rocket.dom.html, 'rme-reveal');
+         if (Rocket.exists(openMenu) && Rocket.is.element(openMenu)) {
+            if (Rocket.has.class(openMenu, '_t-slide')) { Rocket.overlay.hide(); }
+            Rocket.classes.remove(openMenu, '_reveal');
+         }
+         if (Rocket.is.function(callback)) { return callback(); }
+      },
+      globalSetup: () => {
+         Rocket.event.add(Rocket.dom.element('#rocket-overlay'), 'click', menu.close);
+      },
+      setup: options => {
+         const thisMenu = Rocket.dom.element(options.target);
+         const triggers = Rocket.dom.select(options.triggers);
+
+         // Catch
+         if (!Rocket.is.element(thisMenu) || triggers.length < 1 || types.indexOf(`_t-${options.type}`) < 0) { return false; }
+
+         // Continue
+         function menuShow(event = null) {
+            if (event) { event.preventDefault(); }
+
+            menu.close(() => {
+               if (options.type === 'slide') { Rocket.overlay.show(); }
+               Rocket.classes.add(Rocket.dom.html, 'rme-reveal');
+               Rocket.classes.add(thisMenu, '_reveal');
+            });
+         }
+
+         Rocket.classes.remove(thisMenu, reveals.concat(types));
+         switch(options.type) {
+            case 'contextual':
+               Rocket.classes.add(thisMenu, `_t-${options.type}`);
+               break;
+
+            case 'slide':
+               Rocket.classes.add(thisMenu, `_t-${options.type} _r-${options.reveal}`);
+
+               if (!Rocket.exists(thisMenu.querySelector('a.rme-close-link'))) {
+                  let closeLink = document.createElement('a');
+                  Rocket.classes.add(closeLink, 'rme-close-link');
+                  closeLink.href = '';
+                  closeLink.innerHTML = options.closeText;
+                  thisMenu.appendChild(closeLink);
+               }
+               break;
+         }
+
+         // Menu links
+         const menuLinks = thisMenu.querySelectorAll('a');
+         for (let menuLink of menuLinks) {
+            Rocket.event.add(menuLink, 'click', event => {
+               if (Rocket.has.class(event.currentTarget, 'rme-close-link')) { event.preventDefault(); }
+               menu.close();
+            });
+         }
+
+         for (let trigger of triggers) {
+            Rocket.event.add(trigger, 'click', menuShow);
+         }
+
+         return {
+            menu: thisMenu,
+            show: menuShow
+         }
+      }
+   };
+
+   // Initialiser
+   export function init(uOptions) {
+      // Options
+      if (!Rocket.is.object(uOptions)) { uOptions = {}; }
+      const options = {
+         closeText: Rocket.helper.setDefault(uOptions.closeText, _RD.closeText),
+         reveal: Rocket.helper.setDefault(uOptions.reveal, _RD.reveal),
+         triggers: Rocket.helper.setDefault(uOptions.triggers, ''),
+         target: Rocket.helper.setDefault(uOptions.target, ''),
+         type: Rocket.helper.setDefault(uOptions.type, _RD.type)
+      };
+
+      return menu.setup(options);
+   }
+
+   // Menu setup
+   menu.globalSetup();
+}
+
+// Bind to Rocket
+Rocket.menu = RockMod_Menu.init;
